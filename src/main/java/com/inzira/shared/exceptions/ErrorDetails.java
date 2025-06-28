cc9b54979f951f3b58a167d7c4a4a236ff3f180f
Gabiro Vladimir Brenn <103499307+gabbrenn@users.noreply.github.com>
2025-06-29 01:57:34 +0200

    Fix Lombok Compilation Errors

--- src/main/java/com/inzira/shared/exceptions/ErrorDetails.java
+++ src/main/java/com/inzira/shared/exceptions/ErrorDetails.java
@@ -4,9 +4,11 @@ import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // Used for structured error response
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ErrorDetails {
     private LocalDateTime timestamp;
